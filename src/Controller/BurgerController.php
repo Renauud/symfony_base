983fcb7a2899b9a062233408e3afc4f8b617f572
Renaud <rzell57@gmail.com>
2023-11-09 02:06:04 +0100

    ex1 fait, ex2 en cours bientôt terminé

---
+++ src/Controller/BurgerController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BurgerController extends AbstractController
+{
+    #[Route('/burger_list', name: 'app_burger_list')]
+    public function listAll()
+    {
+        return $this->render('burger_list.html.twig', ["aze"]);
+        // dd('liste des burgers');
+    }
+}
